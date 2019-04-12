8881ac51e1731153ea2fcec73f236ca96a26271f
Alex <alexander.t.carter@gmail.com>
2019-04-12 14:52:42 -0700

    updated my nnames.py solution

---
+++ names/binary_search_tree.py
@@ -0,0 +1,55 @@
+
+class BinarySearchTree:
+  def __init__(self, value):
+    self.value = value
+    self.left = None
+    self.right = None
+
+  def insert(self, value):
+    curr_node = self
+    while(True==True):
+        if(value > curr_node.value):
+          if curr_node.right != None:
+            curr_node = curr_node.right
+          else:
+            curr_node.right = BinarySearchTree(value)
+            break
+        else:
+          if curr_node.left != None:
+            curr_node = curr_node.left
+          else:
+            curr_node.left = BinarySearchTree(value)
+            break
+
+  def contains(self, target):
+    curr_node = self
+    value = self.value
+    while(True==True):
+      if(target==curr_node.value):
+        return True
+      else:
+        if(target > curr_node.value):
+          if curr_node.right != None:
+            curr_node = curr_node.right
+          else:
+            return False
+        else:
+          if curr_node.left != None:
+            curr_node = curr_node.left
+          else:
+            return False
+
+  def get_max(self):
+    curr_node = self
+    while(True==True):
+      if curr_node.right != None:
+        curr_node=curr_node.right
+      else:
+        return curr_node.value
+
+  def for_each(self, cb):
+    cb(self.value)
+    if self.right != None:
+      self.right.for_each(cb)
+    if self.left != None:
+      self.left.for_each(cb)
--- names/names.py
+++ names/names.py
@@ -1,7 +1,8 @@
 import time
+from binary_search_tree import BinarySearchTree
 
 start_time = time.time()
-
+bst = BinarySearchTree(0)
 f = open('names_1.txt', 'r')
 names_1 = f.read().split("\n")  # List containing 10000 names
 f.close()
@@ -11,17 +12,34 @@ names_2 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
 '''
+assignment's original code:
+
 duplicates = []
 for name_1 in names_1:
     for name_2 in names_2:
         if name_1 == name_2:
             duplicates.append(name_1)
 '''
+
+def convertNameToNumber(name):
+  numString = ''
+  for c in range(0,len(name)):
+    numString = numString + str(ord(name[c]))
+  return int(numString)
+
+print(convertNameToNumber('Alex'))
+
+
 duplicates = []
+counter = 0
 for name_1 in names_1:
-  #if names_2.index(name_1) != -1:
-  if name_1 in names_2:
-    duplicates.append(name_1)
+  counter+=1
+  print(counter)
+  bst.insert(convertNameToNumber(name_1))
+for name_2 in names_2:
+  if bst.contains(convertNameToNumber(name_2)):
+    duplicates.append(name_2)
+#redo with binary search
 
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
